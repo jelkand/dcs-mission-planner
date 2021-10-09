5265f8995e109db9686903350d8f7fa31ce55fca
Jack Anderson <jack@jackanderson.dev>
2021-10-09 10:48:26 -0400

    set up enums

---
+++ db/migrations/20211009144812_set_up_enums/migration.sql
@@ -0,0 +1,19 @@
+/*
+  Warnings:
+
+  - A unique constraint covering the columns `[hashedToken,type]` on the table `Token` will be added. If there are existing duplicate values, this will fail.
+  - Changed the type of `type` on the `Token` table. No cast exists, the column would be dropped and recreated, which cannot be done if there is data, since the column is required.
+
+*/
+-- CreateEnum
+CREATE TYPE "TokenType" AS ENUM ('RESET_PASSWORD');
+
+-- CreateEnum
+CREATE TYPE "WaypointType" AS ENUM ('STEERPOINT', 'BULLSEYE');
+
+-- AlterTable
+ALTER TABLE "Token" DROP COLUMN "type",
+ADD COLUMN     "type" "TokenType" NOT NULL;
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Token_hashedToken_type_key" ON "Token"("hashedToken", "type");
