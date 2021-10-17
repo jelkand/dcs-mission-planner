32b90c2ca0ebf1d75c722ea31c855f4cb7f69810
Jack Anderson <jack@jackanderson.dev>
2021-10-17 16:20:53 -0400

    prototype socket integration

---
+++ app/dcsIntegration/components/DcsSocketIntegration.tsx
@@ -0,0 +1,47 @@
+import { Button } from "@chakra-ui/button"
+import { Code } from "@chakra-ui/layout"
+import React, { MutableRefObject, useCallback, useEffect, useMemo, useRef, useState } from "react"
+// import { w3cwebsocket as W3CWebSocket } from "websocket"
+import useWebSocket, { ReadyState } from "react-use-websocket"
+
+// const client = new W3CWebSocket(
+// )
+
+const DCS_SOCKET = `${process.env.BLITZ_PUBLIC_DCS_SOCKET_HOST}:${process.env.BLITZ_PUBLIC_DCS_SOCKET_PORT}`
+
+export const DcsSocketIntegration = () => {
+  const [socketUrl, setSocketUrl] = useState(DCS_SOCKET)
+  const messageHistory: MutableRefObject<any> = useRef([])
+
+  const { sendMessage, lastMessage, readyState } = useWebSocket(socketUrl)
+
+  messageHistory.current = useMemo(
+    () => messageHistory?.current?.concat(lastMessage),
+    [lastMessage]
+  )
+
+  const handleClickSendMessage = useCallback(() => sendMessage("Ping"), [])
+
+  const connectionStatus = {
+    [ReadyState.CONNECTING]: "Connecting",
+    [ReadyState.OPEN]: "Open",
+    [ReadyState.CLOSING]: "Closing",
+    [ReadyState.CLOSED]: "Closed",
+    [ReadyState.UNINSTANTIATED]: "Uninstantiated",
+  }[readyState]
+
+  return (
+    <div>
+      <Button onClick={handleClickSendMessage} disabled={readyState !== ReadyState.OPEN}>
+        Click Me to send &apos;Ping&apos;
+      </Button>
+      <span>The WebSocket is currently {connectionStatus}</span>
+      {lastMessage ? <span>Last message: {lastMessage.data}</span> : null}
+      <ul>
+        {messageHistory?.current?.map((message, idx) => (
+          <span key={idx}>{message ? message.data : null}</span>
+        ))}
+      </ul>
+    </div>
+  )
+}
--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -1,33 +1,13 @@
-import { Box, HStack } from "@chakra-ui/layout"
-import logout from "app/auth/mutations/logout"
-import { CoordinateFields } from "app/coordinates/components/CoordinateFields"
-import { CoordinateForm } from "app/coordinates/components/CoordinateForm"
-import { DDMCoordinateForm } from "app/coordinates/components/DDMCoordinateForm"
-import { Direction } from "app/coordinates/Direction"
-import { toDDM } from "app/coordinates/transformers"
-import { DDMCoordinate } from "app/coordinates/validations"
-import Form from "app/core/components/Form"
-import { InlineTextField } from "app/core/components/InlineTextField"
-import { useCurrentUser } from "app/core/hooks/useCurrentUser"
 import Layout from "app/core/layouts/Layout"
-import { BlitzPage, Image, Link, Routes, useMutation } from "blitz"
-import React, { Suspense } from "react"
-import { z } from "zod"
-
-const initialCoord = { latitude: -77.508333, longitude: 164.754167 }
+import { DcsSocketIntegration } from "app/dcsIntegration/components/DcsSocketIntegration"
+import { BlitzPage } from "blitz"
+import React from "react"
 
 const Home: BlitzPage = () => {
   return (
     <div className="container">
       <main>
-        <Form<typeof DDMCoordinate>
-          onSubmit={console.log}
-          initialValues={toDDM(initialCoord)}
-          schema={DDMCoordinate}
-          submitText="Save"
-        >
-          <CoordinateFields name="coordinate" label="label" />
-        </Form>
+        <DcsSocketIntegration></DcsSocketIntegration>
       </main>
 
       <footer></footer>
