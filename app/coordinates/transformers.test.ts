af4ffd38ecbb78441cf2ef6409f538ee1d82c9ea
Jack Anderson <jack@jackanderson.dev>
2021-11-15 21:51:11 -0500

    add transformers to convert waypoints to inputs

--- app/coordinates/transformers.test.ts
+++ app/coordinates/transformers.test.ts
@@ -1,7 +1,13 @@
 import { toMatchCloseTo } from "jest-matcher-deep-close-to"
 expect.extend({ toMatchCloseTo })
 import { Direction } from "./Direction"
-import { ddmToDecimalValue, decimalValueToDDM, toDDM } from "./transformers"
+import {
+  ddmToDecimalValue,
+  decimalCoordToInputCoord,
+  decimalValueToDDM,
+  decimalValueToInputString,
+  toDDM,
+} from "./transformers"
 
 describe("Coordinate Transformers", () => {
   it("Converts a decimal degree coordinate to DDM", () => {
@@ -39,5 +45,46 @@ describe("Coordinate Transformers", () => {
     const actual = ddmToDecimalValue(input)
 
     expect(actual).toBeCloseTo(expected)
+  })
+
+  it("converts a decimal degree to an array of inputs", () => {
+    const initialCoordinate = {
+      latitude: -77.508333,
+      longitude: 164.754167,
+    }
+
+    const expected = {
+      latitude: ["S", "7", "7", "3", "0", "5", "0"],
+      longitude: ["E", "1", "6", "4", "4", "5", "2", "5"],
+    }
+    const actual = decimalCoordToInputCoord(initialCoordinate)
+
+    expect(actual).toStrictEqual(expected)
+  })
+
+  it("correctly pad inputs when the minutes have a leading 0", () => {
+    const mockLat = {
+      direction: Direction.N,
+      degrees: 5,
+      minutes: 4.52,
+    }
+    const mockLong = {
+      direction: Direction.E,
+      degrees: 2,
+      minutes: 2.52,
+    }
+
+    const initialCoordinate = {
+      latitude: ddmToDecimalValue(mockLat),
+      longitude: ddmToDecimalValue(mockLong),
+    }
+
+    const expected = {
+      latitude: ["N", "5", "0", "4", "5", "2"],
+      longitude: ["E", "2", "0", "2", "5", "2"],
+    }
+    const actual = decimalCoordToInputCoord(initialCoordinate)
+
+    expect(actual).toStrictEqual(expected)
   })
 })
--- app/coordinates/transformers.ts
+++ app/coordinates/transformers.ts
@@ -1,3 +1,5 @@
+import { RawWaypoint } from "app/aircraft/hornet/hornetMachine"
+import { HornetKey } from "app/aircraft/hornet/inputs"
 import { Coordinate } from "db"
 
 import { Direction } from "./Direction"
@@ -24,10 +26,9 @@ export interface DDMCoordinate {
 }
 
 // TODO: Refactor to infer northing/easting based on generics
-export const decimalValueToDDM = (decimalDegrees: number, isNorthing: boolean): DDM => {
-  const directions: [Direction, Direction] = isNorthing
-    ? [Direction.N, Direction.S]
-    : [Direction.E, Direction.W]
+export const decimalValueToDDM = (decimalDegrees: number, axis: "northing" | "easting"): DDM => {
+  const directions: [Direction, Direction] =
+    axis === "northing" ? [Direction.N, Direction.S] : [Direction.E, Direction.W]
   const degreesDirection = decimalDegrees > 0 ? directions[0] : directions[1]
   const degreesMagnitude = Math.abs(decimalDegrees)
 
@@ -41,17 +42,29 @@ export const decimalValueToDDM = (decimalDegrees: number, isNorthing: boolean): 
   }
 }
 
-export const decimalValueToDDMString = (decimalDegrees: number, isNorthing: boolean): string => {
-  const { direction, degrees, minutes } = decimalValueToDDM(decimalDegrees, isNorthing)
+export const decimalValueToDDMString = (
+  decimalDegrees: number,
+  axis: "northing" | "easting"
+): string => {
+  const { direction, degrees, minutes } = decimalValueToDDM(decimalDegrees, axis)
   return `${direction} ${degrees}° ${minutes.toFixed(4).padStart(7, "0")}`
+}
+
+export const decimalValueToInputString = (decimalDegrees: number, axis: "northing" | "easting") => {
+  const { direction, degrees, minutes } = decimalValueToDDM(decimalDegrees, axis)
+  return [
+    direction,
+    ...degrees.toString().split(""),
+    ...minutes.toFixed(2).replace(".", "").padStart(4, "0").split(""),
+  ]
 }
 
 export const toDDM = ({
   latitude: decimalLat,
   longitude: decimalLong,
 }: Pick<Coordinate, "latitude" | "longitude">): DDMCoordinate => {
-  const latitude = decimalValueToDDM(decimalLat, true) as DDMLatitude
-  const longitude = decimalValueToDDM(decimalLong, false) as DDMLongitude
+  const latitude = decimalValueToDDM(decimalLat, "northing") as DDMLatitude
+  const longitude = decimalValueToDDM(decimalLong, "easting") as DDMLongitude
 
   return { latitude, longitude }
 }
@@ -60,4 +73,14 @@ export const ddmToDecimalValue = ({ direction, degrees, minutes }: DDM): number 
   const directionCoefficient = positiveDirections.has(direction) ? 1 : -1
   const decimal = minutes / 60
   return directionCoefficient * (degrees + decimal)
+}
+
+export const decimalCoordToInputCoord = ({
+  latitude: decimalLat,
+  longitude: decimalLong,
+}: Pick<Coordinate, "latitude" | "longitude">): RawWaypoint => {
+  const latitude = decimalValueToInputString(decimalLat, "northing") as Array<HornetKey>
+  const longitude = decimalValueToInputString(decimalLong, "easting") as Array<HornetKey>
+
+  return { latitude, longitude }
 }
